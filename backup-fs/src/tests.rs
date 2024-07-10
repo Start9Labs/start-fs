e6fd20112d985d444053e734377421990dfb1503
Sam Sartor <me@samsartor.com>
2024-07-10 15:15:29 -0600

    tokio tests

--- backup-fs/src/tests.rs
+++ backup-fs/src/tests.rs
@@ -1,8 +1,12 @@
 use crate::{BackupFS, BackupFSOptions};
 use fuser::MountOption;
+use std::future::Future;
+use std::path::PathBuf;
 use std::process::Command;
 use std::{fs, io, path::Path};
 use tempdir::TempDir;
+use tokio::runtime::Runtime;
+use tokio::task::JoinSet;
 
 fn with_backupfs(
     data: &Path,
@@ -46,6 +50,25 @@ fn with_backupfs(
         Ok(()) => (),
         Err(err) => std::panic::resume_unwind(err),
     };
+}
+
+fn with_backupfs_async<F: Future<Output = ()> + Send + 'static>(
+    data: &Path,
+    password: String,
+    func: impl FnOnce(PathBuf) -> F,
+    file_size_padding: Option<f64>,
+) {
+    with_backupfs(
+        data,
+        password,
+        move |path| {
+            tokio::runtime::Builder::new_current_thread()
+                .build()
+                .unwrap()
+                .block_on(func(path.to_owned()))
+        },
+        file_size_padding,
+    )
 }
 
 fn tree(path: impl AsRef<Path>, dirs: bool) -> Result<Vec<String>, io::Error> {
@@ -136,6 +159,33 @@ fn preserves_file() {
 }
 
 #[test_log::test]
+fn preserves_directory() {
+    let data = TempDir::new("backupfs_data").unwrap();
+    with_backupfs(
+        data.path(),
+        "ohea".to_owned(),
+        |mnt| {
+            fs::create_dir(mnt.join("a")).unwrap();
+            fs::create_dir(mnt.join("a/b")).unwrap();
+            fs::create_dir(mnt.join("a/c")).unwrap();
+        },
+        None,
+    );
+
+    with_backupfs(
+        data.path(),
+        "ohea".to_owned(),
+        |mnt| {
+            assert_eq!(
+                tree(mnt, true).unwrap(),
+                vec!["a".to_owned(), "a/b".to_owned(), "a/c".to_owned()]
+            )
+        },
+        None,
+    );
+}
+
+#[test_log::test]
 fn checksum() {
     let data = TempDir::new("backupfs_data").unwrap();
     with_backupfs(data.path(), "ohea".to_owned(), |_mnt| {}, None);
@@ -153,7 +203,7 @@ fn checksum() {
 }
 
 #[test_log::test]
-fn backupfs_change_password() {
+fn change_password() {
     let data = TempDir::new("backupfs_data").unwrap();
     with_backupfs(
         data.path(),
@@ -183,5 +233,73 @@ fn backupfs_change_password() {
             assert_eq!(fs::read(mnt.join("a_file")).unwrap().as_slice(), b"foo bar");
         },
         None,
+    );
+}
+
+#[test_log::test]
+fn write_one_file_async() {
+    use tokio::fs;
+    let data = TempDir::new("backupfs_data").unwrap();
+    with_backupfs_async(
+        data.path(),
+        "ohea".to_owned(),
+        |mnt| async move {
+            fs::write(mnt.join("a_file"), "foo bar").await.unwrap();
+            let mut contents = fs::read_dir(&mnt).await.unwrap();
+            loop {
+                match contents.next_entry().await.unwrap() {
+                    Some(entry) => {
+                        if entry.file_name() == "a_file" {
+                            break;
+                        }
+                    }
+                    None => panic!(),
+                }
+            }
+            assert_eq!(
+                fs::read(mnt.join("a_file")).await.unwrap().as_slice(),
+                b"foo bar"
+            );
+        },
+        None,
+    );
+    assert_eq!(tree(data.path().join("inodes"), false).unwrap().len(), 2);
+    assert_eq!(tree(data.path().join("contents"), false).unwrap().len(), 1);
+}
+
+#[test_log::test]
+fn write_many_files_async() {
+    use tokio::fs;
+    let data = TempDir::new("backupfs_data").unwrap();
+    with_backupfs_async(
+        data.path(),
+        "ohea".to_owned(),
+        |mnt| async move {
+            let mut tasks = JoinSet::new();
+            for i in 0..100 {
+                tasks.spawn(fs::write(mnt.join(format!("{i:02}")), format!("= {i}")));
+            }
+            while let Some(res) = tasks.join_next().await {
+                res.unwrap().unwrap();
+            }
+            let mut count = 0;
+            let mut contents = fs::read_dir(&mnt).await.unwrap();
+            while let Some(entry) = contents.next_entry().await.unwrap() {
+                if let Ok(i) = dbg!(entry.file_name()).to_str().unwrap().parse::<usize>() {
+                    assert_eq!(
+                        fs::read(entry.path()).await.unwrap().as_slice(),
+                        format!("= {i}").as_bytes()
+                    );
+                    count += 1;
+                }
+            }
+            assert_eq!(count, 100);
+        },
+        None,
+    );
+    assert_eq!(tree(data.path().join("inodes"), false).unwrap().len(), 101);
+    assert_eq!(
+        tree(data.path().join("contents"), false).unwrap().len(),
+        100
     );
 }
